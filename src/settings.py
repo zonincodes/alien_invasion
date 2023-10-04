6901d5f1c6276b39e5b6257e7dd57280bd5dd9ee
Zonin <business.kariukimartin@gmail.com>
2023-10-04 19:00:57 +0300

    update on settings

--- src/settings.py
+++ src/settings.py
@@ -5,23 +5,24 @@ class Settings:
         """Initialize the game's settings."""
 
         # screen settings
-        self.screen_width = 1320
+        self.screen_width = 1200
         self.screen_height = 720
         self.bg_color = (230, 230, 230)
 
         # Ship settings
         self.ship_speed_factor = 5.0
+        self.ship_limit = 3
 
         # Bullet settings
         self.bullet_speed = 2
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 7
+        self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed = 1
-        self.drop_speed = 10
+        self.alien_speed = 1.0
+        self.drop_speed = 5
         # fleet direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
